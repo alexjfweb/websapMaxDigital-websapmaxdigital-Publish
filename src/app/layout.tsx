bc47589265f06b9eb0656d41a2c6d0179a5c407d
alex jf <alexjfweb@gmail.com>
2025-07-24 20:43:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <ClientProviders>{children}</ClientProviders>
         <Toaster />
       </body>
