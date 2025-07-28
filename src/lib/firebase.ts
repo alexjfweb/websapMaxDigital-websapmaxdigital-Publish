d92681da8090936327da2f8d3917617144a6df35
alex jf <alexjfweb@gmail.com>
2025-07-28 16:32:42 +0000

    persiste el problema de no cargar la aaplicacion crrectamente.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,19 +5,6 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './firebase-config'; // Importar la configuración directamente
 
-// Validación para asegurar que todas las variables de la configuración importada existan
-const missingConfig = Object.entries(firebaseConfig)
-  .filter(([key, value]) => !value)
-  .map(([key]) => key);
-
-if (missingConfig.length > 0) {
-  const errorMessage = `Faltan las siguientes variables en la configuración de Firebase importada: ${missingConfig.join(', ')}.`;
-  console.error(errorMessage);
-  // En un entorno de producción, podrías querer lanzar un error para detener la ejecución.
-  // throw new Error(errorMessage);
-}
-
-
 // Asegura que la inicialización ocurra solo una vez.
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
