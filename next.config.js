e4ae633fa00a0086fc161162a55f8259988a2979
alex jf <alexjfweb@gmail.com>
2025-09-06 04:40:21 +0000

    persiste el problema revisa esta guia de sugerencia:El problema es que e

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,6 +10,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
