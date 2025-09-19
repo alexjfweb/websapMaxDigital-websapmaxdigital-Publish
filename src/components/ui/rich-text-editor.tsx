9b54a0381ce24fbc7af66fa79c8f4f73d7017db6
alex jf <alexjfweb@gmail.com>
2025-09-19 19:31:53 +0000

    persiste el problema mira la imagen , ahora revisa esta guía para ver si

--- src/components/ui/rich-text-editor.tsx
+++ src/components/ui/rich-text-editor.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
 
@@ -12,46 +12,18 @@ interface RichTextEditorProps {
 const RichTextEditor: React.FC<RichTextEditorProps> = ({ value, onChange, placeholder }) => {
   const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
 
-  // Registrar módulos de alineación cuando el componente se monte
-  useEffect(() => {
-    const registerQuillModules = async () => {
-      if (typeof window !== 'undefined') {
-        try {
-          const ReactQuillModule = await import('react-quill');
-          // A robust way to get Quill, works with different module structures
-          const Quill = ReactQuillModule.default?.Quill || (ReactQuillModule as any).Quill;
-          
-          if (Quill) {
-            // Intentar registrar los módulos de alineación
-            try {
-              const AlignClass = Quill.import('attributors/class/align');
-              const AlignStyle = Quill.import('attributors/style/align');
-              
-              if (AlignClass) Quill.register(AlignClass, true);
-              if (AlignStyle) Quill.register(AlignStyle, true);
-            } catch (alignError) {
-              console.warn('Could not register alignment modules, they might be registered already:', alignError);
-            }
-          }
-        } catch (error) {
-          console.warn('Error loading Quill modules for registration:', error);
-        }
-      }
-    };
-    
-    registerQuillModules();
-  }, []);
-
   const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, 3, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      [{ 'align': ['', 'center', 'right', 'justify'] }],
-      [{ 'color': [] }],
-      ['link', 'image'],
-      ['clean']
-    ],
+    toolbar: {
+      container: [
+        [{ 'header': [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        [{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }],
+        [{ 'color': [] }],
+        ['link', 'image'],
+        ['clean']
+      ]
+    },
   };
 
   const formats = [
